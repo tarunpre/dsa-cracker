7a85055c147cec6e2b373832f795a834d48fbced
AMR\ tarunpre <tarun.preet.singh@intel.com>
2025-01-05 22:04:43 -0800

    dp

--- daily_problems/2381_shifting-letters-ii.py
+++ daily_problems/2381_shifting-letters-ii.py
@@ -5,35 +5,29 @@ class Solution(object):
         :type shifts: List[List[int]]
         :rtype: str
         """
-        # initiate empty array
-        darray = [0] * len(s)
+        #initiate empty array
+        lens = len(s)
+        darr = [0] * lens
         for k in shifts:
-            fr = k[0]
-            to = k[1]
-            dr = k[2]
-            if dr == 1:
-                darray[fr] += 1
-                if to + 1 < len(darray):
-                    darray[to+1]  -= 1
+            if k[2] == 1:
+                #forward
+                darr[k[0]] += 1
+                if k[1] + 1 < lens:
+                    darr[k[1]+1] -= 1
             else:
-                darray[fr] -= 1
-                if to + 1 < len(darray):
-                    darray[to+1] += 1
-        result = list(s)
-        number_of_shifts = 0
+                #backward
+                darr[k[0]] -= 1
+                if k[1] + 1 < lens:
+                    darr[k[1]+1] += 1
+        j = ""
+        prev = 0
+        for m in range(lens):
+            prev +=  darr[m]
+            cr_val = ord(s[m]) - ord('a') + prev
+            cr_val %= 26
+            j += chr(ord('a') + cr_val)
+        return j
+sol = Solution()
 
-        # Apply the shifts to the string
-        for i in range(len(s)):
-            number_of_shifts = (
-                number_of_shifts + darray[i]
-            ) % 26  # Update cumulative shifts, keeping within the alphabet range
-            if number_of_shifts < 0:
-                number_of_shifts += 26  # Ensure non-negative shifts
-
-            # Calculate the new character by shifting `s[i]`
-            shifted_char = chr(
-                (ord(s[i]) - ord("a") + number_of_shifts) % 26 + ord("a")
-            )
-            result[i] = shifted_char
-
-        return "".join(result)
+res = sol.shiftingLetters("abc", [[0,0,1]])
+print(res)
