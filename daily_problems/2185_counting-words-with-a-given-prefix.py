7331338109a12246506c9c87a25094a1f18dbd18
AMR\ tarunpre <tarun.preet.singh@intel.com>
2025-01-08 20:34:14 -0800

    DP

---
+++ daily_problems/2185_counting-words-with-a-given-prefix.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def prefixCount(self, words, pref):
+        """
+        :type words: List[str]
+        :type pref: str
+        :rtype: int
+        """
+        return len([x for x in words if x.startswith(pref)])
+        
