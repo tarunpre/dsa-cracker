f10b7ca8cb52ca91617ceed34f487cd31b24478c
AMR\ tarunpre <tarun.preet.singh@intel.com>
2025-01-10 23:48:38 -0800

    hard

--- arrays_and_strings/68_text-justification.py
+++ arrays_and_strings/68_text-justification.py
@@ -5,47 +5,41 @@ class Solution():
         :type maxWidth: int
         :rtype: List[str]
         """
-        def rm(rem, k):
-            while rem > 0:
-                for i in range(len(k)):
-                    if " " in k[i]:
-                        if rem:
-                            k[i] += " "
-                            rem -= 1
-                        else:
-                            break
-            return k
-
         ls = []
         s = ""
         for w in range(len(words)):
             if not s:
                 s = words[w]
             else:
-                if not len(s + f" {words[w]}") > maxWidth:
-                    s += f" {words[w]}"
+                if len(s) + 1 + len(words[w]) <= maxWidth:
+                    s += " "
+                    s += words[w]
                 else:
-                    rem = maxWidth - len(s)
-                    k = list(s)
-                    if rem:
-                        k = rm(rem, k)
-                    s = "".join(k)
                     ls.append(s)
                     s = words[w]
-                    if w == len(words) - 1:
-                        rem = maxWidth - len(words[w])
-                        k = list(words[w])
+                if w == len(words) -1 :
+                    ls.append(s)
+        if s and not ls:
+            ls.append(s)
+        for l in range(len(ls)):
+            rem = maxWidth - len(ls[l])
+            if rem: 
+                if " " in ls[l] and l != len(ls)-1:
+                    k = list(ls[l])
+                    ind = [j for j in range(len(k)) if " " in k[j]]
+                    while rem > 0:
+                        for m in ind:
+                            if rem:
+                                k[m] += " "
+                                rem -= 1
+                    ls[l] = "".join(k)
+                else:
+                    k = list(ls[l])
+                    while rem > 0:
                         k.append(" ")
                         rem -= 1
-                        if rem:
-                            k = rm(rem, k)
-                        ls.append("".join(k))
-
-
-
-
-                    
-                    
-        
+                    ls[l]="".join(k)
+        return ls
 sol = Solution()
-sol.fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16)
+#sol.fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16)
+sol.fullJustify(["What","must","be","acknowledgment","shall","be"], 16)
