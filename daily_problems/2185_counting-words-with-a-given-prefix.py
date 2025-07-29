273634e44cfa27262d852a9fca22eaf6ae0f0db5
AMR\ tarunpre <tarun.preet.singh@intel.com>
2025-07-29 10:28:14 -0700

    cleanup

--- daily_problems/2185_counting-words-with-a-given-prefix.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def prefixCount(self, words, pref):
-        """
-        :type words: List[str]
-        :type pref: str
-        :rtype: int
-        """
-        return len([x for x in words if x.startswith(pref)])
-        
--- matrix/test.py
+++
@@ -1,5 +0,0 @@
-matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
-
-for i in matrix:
-    for j in i:
-        print(i,j)
